0fca90f911f43c4c3e730c62596876301379434b
Charaka V Mihiranga <143955766+CharakaMihiranga@users.noreply.github.com>
2024-10-28 22:52:30 +0530

    initialize project structure and add health check API

---
+++ src/main/java/lk/ijse/springboot/CropMonitorAPI/CropMonitorApiApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.springboot.CropMonitorAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CropMonitorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CropMonitorApiApplication.class, args);
+	}
+
+}
